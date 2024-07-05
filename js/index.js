ba600485504d6b9799f35ae43ba5c6ccef2daf12
Shreyas Gopnarayan <gopnarayanshreyas2004@gmail.com>
2024-07-05 08:37:06 +0530

    contact us page added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 http://feedcalcsoft.com/
+
+Mobile devices – 320px — 480px.
+iPads, Tablets – 481px — 768px.
+Small screens, laptops – 769px — 1024px.
+Desktops, large screens – 1025px — 1200px.
+Extra large screens, TV – 1201px, and more.

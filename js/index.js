03adf1eb925827d6a9ed08638c67f25930a1df47
Shreyas Gopnarayan <gopnarayanshreyas2004@gmail.com>
2024-08-02 10:49:56 +0530

    contact us updated

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-http://feedcalcsoft.com/
+// http://feedcalcsoft.com/
 
-Mobile devices – 320px — 480px.
-iPads, Tablets – 481px — 768px.
-Small screens, laptops – 769px — 1024px.
-Desktops, large screens – 1025px — 1200px.
-Extra large screens, TV – 1201px, and more.
+// Mobile devices – 320px — 480px.
+// iPads, Tablets – 481px — 768px.
+// Small screens, laptops – 769px — 1024px.
+// Desktops, large screens – 1025px — 1200px.
+// Extra large screens, TV – 1201px, and more.
+
+const download = () => {
+    window.location.replace = "http://download.feedcalcsoft.com/";
+}
